905061bfe5635536d185df59466381790c5fff89
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-08-21 16:56:06 +0800

    PDC20 MOD1Registration Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Parent from './Parent';
-import Registration from './Registration';
+import MOD1Registration from './MOD1Registration';
+
 
 //import the .js file then call it using the "<.js/>" ie. <HelloWorld/>
 
 function App() {
   return (
     <div>
+      <MOD1Registration/>
       <Parent/>
-      <Registration/>
     </div>
     
   );
