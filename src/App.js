e8d09b1db5c3098b3f6251f9d6f0812896aa83fa
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-08-21 16:03:00 +0800

    Registration CSS Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Parent from './Parent';
-
+import Registration from './Registration';
 
 //import the .js file then call it using the "<.js/>" ie. <HelloWorld/>
 
 function App() {
   return (
-    <Parent/>
+    <div>
+      <Parent/>
+      <Registration/>
+    </div>
+    
   );
 }
 
--- src/Registration.js
+++ src/Registration.js
@@ -8,13 +8,58 @@ function Registration() {
     const [password, setPassword] = useState('');
     const [submitted, setSubmitted] = useState(false); //default value is false
 
+    //to prevent the default
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submitted:', {username, email, password});
+        setSubmitted(true);
+    };
+
     return (
         <div className="Registration">
             <header className="App-header">
-                <label htmlFor='username'>Username :</label>
-                <input type = 'text' id = 'username' value = "" required />
+                <h1>Registration Form</h1>
+                <form onSubmit={handleSubmit} className='registration-form'>
+                    <div>
+                        <label htmlFor='username'>Username :</label>
+                        <input
+                        type = 'text' 
+                        id = 'username' 
+                        value = {username}
+                        onChange = {(e) => setUsername(e.target.value)}
+                        required />
+                    </div>
+
+                    <div>
+                        <label htmlFor='email'>Email :</label>
+                        <input
+                        type = 'text' 
+                        id = 'email' 
+                        value = {email}
+                        onChange = {(e) => setEmail(e.target.value)}
+                        required />
+                    </div>
+
+                    <div>
+                        <label htmlFor='password'>Password :</label>
+                        <input
+                        type = 'password' 
+                        id = 'password' 
+                        value = {password}
+                        onChange = {(e) => setPassword(e.target.value)}
+                        required />
+                    </div>
+
+                    <button type='submit'>Register</button>
+                </form>
+
+                {submitted && (
+                    <p className='success-message'>Registration Successful!</p>
+                )}
+
             </header>
         </div>
+
     );
 }
 
